4fb43d6e5df2a18c4db23ea537e892ad5456f198
akhil <akhilgolla003@gmail.com>
2023-11-07 18:29:53 -0600

    added favicon img

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,11 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'google clone',
   description: 'google clone created by next js13',
+  
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body className={inter.className}>{children}</body>
     </html>
   )
