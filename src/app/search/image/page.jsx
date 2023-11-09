193aa7f63723eb51463109f863985b5f1d23c195
akhil <akhilgolla003@gmail.com>
2023-11-08 19:42:51 -0600

    create serach page for web and image

---
+++ src/app/search/image/page.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      Image Search page
+    </div>
+  )
+}
---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function SearchHeader() {
+  return (
+    <div>
+      Seach Header
+    </div>
+  )
+}
