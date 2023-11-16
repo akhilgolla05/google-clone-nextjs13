bdccea7c873738dbf8ce131874a967d293943b03
gollaakhil1115@gmail.com <akhilgolla003@gmail.com>
2023-11-15 19:46:25 -0600

    remove waiting time to load pages

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -7,10 +7,12 @@ import ImageSearchResults from '@/components/ImageSearchResults'
 
 export default async function page({searchParams}) {
 
-  await new Promise((resolve)=>setTimeout(resolve,5000))
+  const startIndex = searchParams.start || "1"
+
+  // await new Promise((resolve)=>setTimeout(resolve,5000))
   
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
   )
 
   if(!response.ok)
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default async function page({searchParams}) {
 
   const startIndex = searchParams.start || "1"
-  await new Promise((resolve)=>setTimeout(resolve,5000))
+  // await new Promise((resolve)=>setTimeout(resolve,5000))
   
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
